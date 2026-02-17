67591190479d7d32f74be4a16c9b51f19b972693
J.Serolina <serolinaj@gmail.com>
2026-02-17 14:44:44 +0530

    Refactor Appointment and Contact admin registration
    Refactor admin registration for Appointment and Contact models.

--- myreminders/admin.py
+++ myreminders/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Appointment,Contact
-admin.site.register(Appointment)
-admin.site.register(Contact)    
+
+admin.site.register(Contact)  
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display=('name','email','date','time','status')  
 
 # Register your models here.
