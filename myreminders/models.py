1d3fa7bf654517fa1a372b5d32825b262a89f5fa
J.Serolina <serolinaj@gmail.com>
2026-02-17 14:43:50 +0530

    Enhance Appointment model with service and status choices
    Added service and updated status fields with choices for Appointment model.

--- myreminders/models.py
+++ myreminders/models.py
@@ -1,14 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 class Appointment(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     date=models.DateField()
     time=models.TimeField()
     phone=models.CharField(max_length=20)
-    status=models.CharField(max_length=20)
+
+    SERVICE_CHOICES=[('Doctor','Doctor'),
+                     ('Office Interview','Office Interview'),
+                     ('Business Meeting','Business Meeting'),
+                    ('Beauty Parlour','Beauty Parlour'),
+                    ]
+    service=models.CharField(max_length=50,choices=SERVICE_CHOICES)
+    
+    STATUS_CHOICES=[('Pending','Pending'),
+                    ('Approved','Approved'),
+                    ('Rejected','Rejected'),
+                    ('Completed','Completed'),]
+    status=models.CharField(max_length=20,choices=STATUS_CHOICES, default='Pending')
 
     def __str__(self):
         return self.name
+    
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
@@ -16,3 +31,4 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+    
